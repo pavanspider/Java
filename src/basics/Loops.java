8c0f978d1dc10c596e56c867da2ffc9fc3b28113
pavan <juvarya@Juvaryas-MacBook-Air.local>
2024-02-06 21:51:14 +0530

    Initial commit

---
+++ src/basics/Loops.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class Loops {
+
+    public static void loops(){
+        String name = "Name";
+        String name1 = "name";
+        if (name.equalsIgnoreCase(name)){
+           System.out.println(name);
+        }else {
+            System.out.println(name1);
+        }
+
+    }
+    public static void main(String[] args) {
+        Loops.loops();
+    }
+}
